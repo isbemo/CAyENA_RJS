a83c1af24d2272dc916d03aadb08fe21abb19161
sdse95 <sdse95@gmail.com>
2021-11-28 21:42:22 -0500

    Update users.controller.js

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -7,15 +7,51 @@ userCtrl.getUsers = async(req,res)=> {
 }
 
 userCtrl.createUser = async (req,res)=> {
-	const { username } = req.body;
-	const newUser = new User({username});
+	const { username, phone, password } = req.body;
+	const newUser = new User({
+		username,
+		phone,
+		password});
 	await newUser.save ()
 	res.json('User create')
 }
 
 userCtrl.deleteUser = async (req, res) => {
-    await User.findByIdAndDelete(req.params.id);
-    res.json('Note Deleted');
+    await User.findByIdAndDelete(req.params.id)
+    res.json('User Deleted');
+}
+
+
+userCtrl.login = async(req, res) => {
+    const { username, password} = req.body;
+    const admin = await User.findOne({username: username});
+    if ( !admin ) {
+        return res.json({
+            mensaje: 'Correo incorrecto'
+        })
+    }
+
+    const match = await (password, admin.password);
+
+    if (match) {
+
+	    if (admin.password == password)
+		    {
+		    	res.json({
+		            mensaje: 'Autenticación exitosa',
+		            id: admin._id,
+		            nombre: admin.nombre,
+		        })
+		    } else {
+        res.json({
+            mensaje: 'Contraseña incorrecta'
+        	})
+    	}     
+    } else{
+    	res.json({
+    		mensaje: 'No se encuentra Contraseña'
+    	})
+    }
 }
 
 module.exports = userCtrl;
