beb7e572879d93c515db718ec5abcfd28866ea3c
sdse95 <sdse95@gmail.com>
2021-11-28 21:41:44 -0500

    Update users.js

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 const router = Router();
-const { getUsers, createUser, deleteUser } = require('../controllers/users.controller');
+const { getUsers, createUser, deleteUser, login } = require('../controllers/users.controller');
 
 router.route('/')
 	.get(getUsers)
 	.post(createUser);
 
+
 router.route('/:id')
 	.delete(deleteUser);
+
+router.route('/login')
+	.get(login);
 
 module.exports = router;
